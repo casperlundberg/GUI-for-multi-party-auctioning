2c980e2e8dd9795353ad7277dadaef1ba08668a5
quran <guustavc@hotmail.com>
2021-01-25 14:33:06 +0100

    BOXES

--- new_project/lib/Dashboard/mainGUI.dart
+++ new_project/lib/Dashboard/mainGUI.dart
@@ -3,14 +3,31 @@ import 'package:flutter/material.dart';
 class MainGUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Center(
-        child: SizedBox(
-          width: 1000,
-          child: Card(),
+    return Row(
+      mainAxisAlignment:
+          MainAxisAlignment.center, //Center Row contents horizontally
+      crossAxisAlignment:
+          CrossAxisAlignment.center, //Center Row contents vertically
+      children: [
+        Container(
+          color: Colors.orange,
+          child: FlutterLogo(
+            size: 400.0, //Insert 1st class
+          ),
         ),
-      ),
+        Container(
+          color: Colors.blue,
+          child: FlutterLogo(
+            size: 400.0, //Insert 2nd class
+          ),
+        ),
+        Container(
+          color: Colors.purple,
+          child: FlutterLogo(
+            size: 400.0, //Insert 3rd class
+          ),
+        ),
+      ],
     );
   }
 }
