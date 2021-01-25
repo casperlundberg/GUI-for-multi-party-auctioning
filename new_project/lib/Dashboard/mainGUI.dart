3abe6848699d311e200c92fe5a9d3f71e3878396
quran <guustavc@hotmail.com>
2021-01-25 15:21:34 +0100

    update

--- new_project/lib/Dashboard/mainGUI.dart
+++ new_project/lib/Dashboard/mainGUI.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
+import '../navigationbar/navbar.dart';
 
 class MainGUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment:
-          MainAxisAlignment.center, //Center Row contents horizontally
-      crossAxisAlignment:
-          CrossAxisAlignment.center, //Center Row contents vertically
-      children: [
-        Container(
-          color: Colors.orange,
-          child: FlutterLogo(
-            size: 400.0, //Insert 1st class
-          ),
+    return new Scaffold(
+        appBar: new AppBar(
+          title: const Text('AppBar Demo'),
+          backgroundColor: const Color(0xFF0099a9),
         ),
-        Container(
-          color: Colors.blue,
-          child: FlutterLogo(
-            size: 400.0, //Insert 2nd class
+        body: Column(children: <Widget>[
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.center, //Center Row contents horizontally
+            crossAxisAlignment:
+                CrossAxisAlignment.center, //Center Row contents vertically
+            children: [
+              Container(
+                color: Colors.orange,
+                margin: EdgeInsets.all(25.0),
+                child: FlutterLogo(
+                  size: 400.0,
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                margin: EdgeInsets.all(25.0),
+                child: FlutterLogo(
+                  size: 400.0,
+                ),
+              ),
+              Container(
+                color: Colors.purple,
+                margin: EdgeInsets.all(25.0),
+                child: FlutterLogo(
+                  size: 400.0,
+                ),
+              ),
+            ],
           ),
-        ),
-        Container(
-          color: Colors.purple,
-          child: FlutterLogo(
-            size: 400.0, //Insert 3rd class
-          ),
-        ),
-      ],
-    );
+        ]));
   }
 }
