ee9155f47d03ce5191ed6b55d2b08790993799f2
Tinkermandota <erisal-8@student.ltu.se>
2021-01-25 14:23:13 +0100

    merge

--- new_project/lib/main.dart
+++ new_project/lib/main.dart
@@ -21,9 +21,18 @@ class MyApp extends StatelessWidget {
         // Define the default TextTheme. Use this to specify the default
         // text styling for headlines, titles, bodies of text, and more.
         textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          headline1: TextStyle(
+            fontSize: 72.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline6: TextStyle(
+            fontSize: 36.0,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 14.0,
+            fontFamily: 'Hind',
+          ),
         ),
       ),
     );
