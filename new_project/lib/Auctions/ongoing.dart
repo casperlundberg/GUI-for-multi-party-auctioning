3eaa274199ce91bf463e1a1960f7856d8a3cea7b
quran <guustavc@hotmail.com>
2021-01-26 14:06:25 +0100

    .

--- new_project/lib/Auctions/ongoing.dart
+++ new_project/lib/Auctions/ongoing.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
 import 'AuctionsTemplateGUI.dart';
+import 'FiltersTemplateGUI.dart';
 
 class Ongoing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 325.0,
-      height: 700.0,
-      child: AuctionTemplateGUI(),
+    return new Container(
+      color: Colors.grey[900],
+      child: new Column(
+        children: <Widget>[
+          Column(children: [
+            new Container(
+              width: 320.0,
+              height: 110.0,
+              child: Text('s'),
+              color: Colors.orange,
+              margin: EdgeInsets.all(5.0),
+            ),
+          ]),
+          Column(children: [
+            new Container(
+              child: Text('s'),
+              width: 320.0,
+              height: 110.0,
+              color: Colors.green,
+              margin: EdgeInsets.all(5.0),
+            ),
+          ]),
+          Column(children: [
+            new Container(
+              child: Text('s'),
+              width: 320.0,
+              height: 110.0,
+              color: Colors.green,
+              margin: EdgeInsets.all(5.0),
+            ),
+          ]),
+          Column(children: [
+            new Container(
+              child: Text('s'),
+              width: 320.0,
+              height: 110.0,
+              color: Colors.green,
+              margin: EdgeInsets.all(5.0),
+            ),
+          ]),
+          Column(children: [
+            new Container(
+              child: Text('s'),
+              width: 320.0,
+              height: 110.0,
+              color: Colors.green,
+              margin: EdgeInsets.all(5.0),
+            ),
+          ]),
+          Column(children: [
+            new Container(
+              child: AuctionTemplateGUI(),
+              width: 320.0,
+              height: 110.0,
+              margin: EdgeInsets.all(5.0),
+            ),
+          ]),
+        ],
+      ),
     );
   }
 }
