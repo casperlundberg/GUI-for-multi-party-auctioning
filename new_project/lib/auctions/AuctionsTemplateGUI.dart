2ce6f91cc8af044fd13dff11e2ff73f5354ebd93
Casper Lundberg <cappelumpa@gmail.com>
2021-01-26 01:45:18 +0100

    fixed responsive issues on auctiontemplategui

--- new_project/lib/auctions/AuctionsTemplateGUI.dart
+++ new_project/lib/auctions/AuctionsTemplateGUI.dart
@@ -7,11 +7,15 @@ class AuctionTemplateGUI extends StatefulWidget {
 
 class _AuctionTemplateState extends State<AuctionTemplateGUI> {
   final _formKey = GlobalKey<FormState>();
+  TextEditingController _textEditingControllerOne;
+  TextEditingController _textEditingControllerTwo;
+  TextEditingController _textEditingControllerThree;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        // ignore: deprecated_member_use
         child: RaisedButton(
           onPressed: () {
             showDialog(
@@ -36,46 +40,90 @@ class _AuctionTemplateState extends State<AuctionTemplateGUI> {
                         ),
                         Form(
                           key: _formKey,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
+                          child: Wrap(
+                            alignment: WrapAlignment.center,
+                            crossAxisAlignment: WrapCrossAlignment.center,
                             children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.all(1.0),
-                                child: TextFormField(
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                    labelText: '<amount>',
-                                    prefix: Text('I hereby accept to pay '),
-                                    suffix: Text(' sek for exchange of '),
+                              Flexible(
+                                child: IntrinsicWidth(
+                                  child: Text(
+                                    'I hereby accept to pay ',
+                                    textAlign: TextAlign.center,
+                                    softWrap: true,
                                   ),
                                 ),
                               ),
-                              Padding(
-                                padding: EdgeInsets.all(1.0),
-                                child: TextFormField(
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                    labelText: '<quantity>',
+                              Flexible(
+                                child: IntrinsicWidth(
+                                  child: TextFormField(
+                                    controller: _textEditingControllerOne,
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      focusedBorder: InputBorder.none,
+                                      enabledBorder: InputBorder.none,
+                                      errorBorder: InputBorder.none,
+                                      disabledBorder: InputBorder.none,
+                                      hintText: '<amount>',
+                                    ),
                                   ),
                                 ),
                               ),
-                              Padding(
-                                padding: EdgeInsets.all(1.0),
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: '<object>',
+                              Expanded(
+                                child: Text(
+                                  ' sek for exchange of ',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Flexible(
+                                child: IntrinsicWidth(
+                                  child: TextFormField(
+                                    controller: _textEditingControllerTwo,
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      focusedBorder: InputBorder.none,
+                                      enabledBorder: InputBorder.none,
+                                      errorBorder: InputBorder.none,
+                                      disabledBorder: InputBorder.none,
+                                      hintText: '<quantity>',
+                                    ),
                                   ),
                                 ),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: RaisedButton(
-                                  child: Text("Create"),
-                                  onPressed: () {
-                                    if (_formKey.currentState.validate()) {
-                                      _formKey.currentState.save();
-                                    }
-                                  },
+                              Flexible(
+                                child: IntrinsicWidth(
+                                  child: TextFormField(
+                                    controller: _textEditingControllerThree,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      focusedBorder: InputBorder.none,
+                                      enabledBorder: InputBorder.none,
+                                      errorBorder: InputBorder.none,
+                                      disabledBorder: InputBorder.none,
+                                      hintText: '<object>\n',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Expanded(
+                                // ignore: deprecated_member_use
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: RaisedButton(
+                                    child: Text("Create"),
+                                    onPressed: () {
+                                      if (_formKey.currentState.validate()) {
+                                        _formKey.currentState.save();
+                                      }
+                                    },
+                                  ),
                                 ),
                               )
                             ],
