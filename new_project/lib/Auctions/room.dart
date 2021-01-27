79bc6b06f2aef22768600f77b5790dfc5e6ff561
quran <guustavc@hotmail.com>
2021-01-27 13:12:45 +0100

    rooms

--- new_project/lib/Auctions/room.dart
+++ new_project/lib/Auctions/room.dart
@@ -23,30 +23,68 @@ class Room extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    'ROOM \n',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    'Name: Project \n',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  Text(
-                    'E-mail: projectgroup13@ltu.se \n',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  Text(
-                    'a new line \n',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  Text(
-                    'a new line \n',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  Text(
-                    'a new line \n',
-                    style: TextStyle(fontSize: 15),
-                  ),
+                  Column(children: [
+                    new Container(
+                      child: Text(
+                        'ROOM NAME: GROUP13, ROOM CODE: 1337',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      width: 1400.0,
+                      height: 40.0,
+                      color: Colors.blue,
+                      margin: EdgeInsets.all(5.0),
+                    ),
+                  ]),
+                  Column(children: [
+                    new Container(
+                      child: Text(
+                        'REQUESTED SERVICE: XXX',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      width: 1400.0,
+                      height: 100.0,
+                      color: Colors.red,
+                      margin: EdgeInsets.all(5.0),
+                    ),
+                  ]),
+                  Column(children: [
+                    Row(children: [
+                      new Container(
+                        child: Text('HOST'),
+                        width: 320.0,
+                        height: 110.0,
+                        color: Colors.grey,
+                        margin: EdgeInsets.all(5.0),
+                      ),
+                    ]),
+                    Row(children: [
+                      new Container(
+                        child: Text('USER 1'),
+                        width: 320.0,
+                        height: 110.0,
+                        color: Colors.yellow,
+                        margin: EdgeInsets.all(5.0),
+                      ),
+                    ]),
+                    Row(children: [
+                      new Container(
+                        child: Text('USER 2'),
+                        width: 320.0,
+                        height: 110.0,
+                        color: Colors.yellow,
+                        margin: EdgeInsets.all(5.0),
+                      ),
+                    ]),
+                    Row(children: [
+                      new Container(
+                        child: Text('USER 3'),
+                        width: 320.0,
+                        height: 110.0,
+                        color: Colors.yellow,
+                        margin: EdgeInsets.all(5.0),
+                      ),
+                    ]),
+                  ]),
                 ],
               ),
             )));
