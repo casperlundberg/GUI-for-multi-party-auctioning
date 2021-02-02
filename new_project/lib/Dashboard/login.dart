209a979d6c8ca962d4a7afcbbd1fb8add2bbf315
Tinkermandota <erisal-8@student.ltu.se>
2021-02-02 07:46:25 +0100

    fix

--- new_project/lib/Dashboard/login.dart
+++ new_project/lib/Dashboard/login.dart
@@ -10,80 +10,52 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-            child: SizedBox(
-                width: 500,
-                child: Card(
-                    child: Form(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Login',
-                          style: Theme.of(context).textTheme.headline4),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(hintText: 'Username'),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(hintText: 'Password'),
-                        ),
-                      ),
-                      TextButton(
-                          child: Text('Signup'),
-                          onPressed: () {
-                            this.parent.setState(() {
-                              this.parent.selectedWidgetMarker =
-                                  WidgetMarker.register;
-                            });
-                          }),
-                      TextButton(
-                          child: Text('Login'),
-                          onPressed: () {
-                            this.parent.setState(() {
-                              this.parent.selectedWidgetMarker =
-                                  WidgetMarker.home;
-                            });
-                          }),
-                      TextButton(
-                          child: Text('Forgot password?'),
-                          onPressed: () {
-                            this.parent.setState(() {
-                              this.parent.selectedWidgetMarker =
-                                  WidgetMarker.forgotPass;
-                            });
-                          }),
-                    ],
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: SizedBox(
+          width: 500,
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Login', style: Theme.of(context).textTheme.headline4),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(hintText: 'Username'),
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(hintText: 'Password'),
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
                   ),
-                  TextButton(
-                      child: Text('Signup'),
-                      onPressed: () {
-                        this.parent.setState(() {
-                          this.parent.selectedWidgetMarker =
-                              WidgetMarker.register;
-                        });
-                      }),
-                  TextButton(
-                      child: Text('Login'),
-                      onPressed: () {
-                        this.parent.setState(() {
-                          this.parent.selectedWidgetMarker = WidgetMarker.home;
-                        });
-                      }),
-                ],
-              ),
+                ),
+                TextButton(
+                    child: Text('Signup'),
+                    onPressed: () {
+                      this.parent.setState(() {
+                        this.parent.selectedWidgetMarker =
+                            WidgetMarker.register;
+                      });
+                    }),
+                TextButton(
+                    child: Text('Login'),
+                    onPressed: () {
+                      this.parent.setState(() {
+                        this.parent.selectedWidgetMarker = WidgetMarker.home;
+                      });
+                    }),
+                TextButton(
+                    child: Text('Forgot password?'),
+                    onPressed: () {
+                      this.parent.setState(() {
+                        this.parent.selectedWidgetMarker =
+                            WidgetMarker.forgotPass;
+                      });
+                    }),
+              ],
             ),
           ),
         ),
