6ba941f43eaf94edf04851040d8dca36e30f3b39
Tinkermandota <erisal-8@student.ltu.se>
2021-02-11 13:33:15 +0100

    createauction

--- new_project/lib/Auctions/auctions.dart
+++ new_project/lib/Auctions/auctions.dart
@@ -38,14 +38,21 @@ class _AuctionsState extends State<Auctions> with SingleTickerProviderStateMixin
     final ThemeData themeData = Theme.of(context);
 
     return Container(
-        height: MediaQuery.of(context).size.height * 0.9,
-        width: MediaQuery.of(context).size.width * 0.4,
-        color: Colors.grey[900],
-        margin: EdgeInsets.all(5.0),
-        child: CustomScrollView(slivers: <Widget>[
+      height: MediaQuery.of(context).size.height * 0.9,
+      width: MediaQuery.of(context).size.width * 0.4,
+      color: Colors.grey[900],
+      margin: EdgeInsets.all(5.0),
+      child: CustomScrollView(
+        slivers: <Widget>[
           SliverAppBar(
             pinned: true,
             title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                onPressed: () {
+                  showContractTemplateGUI();
+                },
+                child: Text("Create auction", style: TextStyle(color: Colors.white)),
+              ),
               Container(
                   width: MediaQuery.of(context).size.width * 0.15,
                   height: MediaQuery.of(context).size.width * 0.05,
@@ -79,7 +86,9 @@ class _AuctionsState extends State<Auctions> with SingleTickerProviderStateMixin
           FutureBuilder(builder: (BuildContext context, AsyncSnapshot snaptshot) {
             return _getPageContainer();
           })
-        ]));
+        ],
+      ),
+    );
   }
 
   SliverFixedExtentList _getPageContainer() {
@@ -140,5 +149,154 @@ class _AuctionsState extends State<Auctions> with SingleTickerProviderStateMixin
           },
           childCount: 10,
         ));
+  }
+
+  int templateItemCount = 1;
+  List<TextEditingController> controllers = [TextEditingController(), TextEditingController()];
+  List<String> types = ["Text", "Number", "Integer"];
+  List<String> dropdownValues = ["Text"];
+
+  void showContractTemplateGUI() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final ThemeData themeData = Theme.of(context);
+
+        return Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+          child: Container(
+            color: themeData.primaryColor,
+            width: MediaQuery.of(context).size.width * 0.2,
+            margin: EdgeInsets.only(left: 0.0, right: 0.0),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(
+                    top: 18.0,
+                  ),
+                  margin: EdgeInsets.only(top: 13.0, right: 8.0),
+                  decoration: BoxDecoration(
+                    //color: Colors.red,
+                    color: Colors.grey[900], //Couldn't import from theme as "Dialog" is transparent
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(16.0),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 0.0,
+                        offset: Offset(0.0, 0.0),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Expanded(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.2,
+                          child: StatefulBuilder(
+                            builder: (context, setState) {
+                              return Row(
+                                children: [
+                                  TextField(
+                                    controller: controllers[0],
+                                  ),
+                                  ListView.builder(
+                                    itemCount: templateItemCount,
+                                    itemBuilder: (context, index) {
+                                      return Row(
+                                        children: [
+                                          DropdownButton(
+                                            icon: Icon(Icons.arrow_downward),
+                                            iconSize: 24,
+                                            value: dropdownValues[index],
+                                            elevation: 16,
+                                            style: TextStyle(color: Colors.white),
+                                            onChanged: (String newValue) {
+                                              setState(() {
+                                                dropdownValues[index] = newValue;
+                                              });
+                                            },
+                                            items: types.map<DropdownMenuItem<String>>((String value) {
+                                              return DropdownMenuItem<String>(
+                                                value: value,
+                                                child: Text(value),
+                                              );
+                                            }).toList(),
+                                          ),
+                                          TextField(
+                                            controller: controllers[index + 1],
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        templateItemCount++;
+                                        controllers.add(TextEditingController());
+                                        dropdownValues.add("Text");
+                                      });
+                                    },
+                                    child: Text("New variable"),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24.0),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ElevatedButton(
+                          child: Text("Create auction"),
+                          onPressed: () {
+                            setState(() {
+                              templateItemCount = 1;
+                              controllers = [TextEditingController(), TextEditingController()];
+                              dropdownValues = ["Text"];
+                            });
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 0.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        templateItemCount = 1;
+                        controllers = [TextEditingController(), TextEditingController()];
+                        dropdownValues = ["Text"];
+                      });
+                      Navigator.of(context).pop();
+                    },
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: CircleAvatar(
+                        radius: 14.0,
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.close, color: Colors.red),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
