a0294a4a223dbc8ea91db0da75abe051c22bd220
Gustav Curan <guustavc@hotmail.com>
2021-02-25 16:40:24 +0100

    Title change

--- new_project/lib/Navigation/navbar.dart
+++ new_project/lib/Navigation/navbar.dart
@@ -15,7 +15,7 @@ class NavigationBar extends StatelessWidget implements PreferredSizeWidget {
       //color cheat sheet
       //https://api.flutter.dev/flutter/material/Colors-class.html
       backgroundColor: themeData.primaryColor,
-      title: Text("Auctioneer"),
+      title: Text("GUI for multi-party auctioning"),
       leading: IconButton(
         icon: Icon(Icons.home),
         tooltip: 'Home',
